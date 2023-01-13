a363b0a5efe396b5a15cb0541a94a723431e3063
anurag4626 <anurag.sharma4626@gmail.com>
2023-01-13 21:35:29 +0530

    This is my test

---
+++ service.ps1
@@ -0,0 +1,17 @@
+﻿configuration Name
+{
+    # One can evaluate expressions to get the node list
+    # E.g: $AllNodes.Where("Role -eq Web").NodeName
+    node ("Node1","Node2","Node3")
+    {
+        # Call Resource Provider
+        # E.g: WindowsFeature, File
+        File FriendlyName
+        {
+            Ensure          = "Present"
+            SourcePath      = $SourcePath
+            DestinationPath = $DestinationPath
+            Type            = "Directory"
+        }       
+    }
+}
